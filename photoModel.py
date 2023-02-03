fc905f73c4ba15646e60ddbede43c5dfb9629f78
pranav <pranavsworkemail@gmail.com>
2023-02-03 09:34:34 -0500

    created model file

---
+++ photoModel.py
@@ -0,0 +1,11 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import ImageDataGenator#prolly wont need this one
+
+import cv2
+import matplotlib as plt
+
+
