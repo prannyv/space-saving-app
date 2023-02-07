d2f879243cde73e01ad72d2447f555bba0bd04ef
pranav <pranavsworkemail@gmail.com>
2023-02-07 00:02:17 -0500

    Update photoModel.py

--- photoModel.py
+++ photoModel.py
@@ -9,3 +9,15 @@ import cv2
 import matplotlib as plt
 
 
+img_height = 28
+img_width = 28
+batch_size = 2
+
+model = keras.Sequential([
+    layers.Input((28,28,1)),
+    layers.Conv2D(16,3,padding='same'),
+    layers.Conv2D(32,3,padding='same'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(10)
+])
