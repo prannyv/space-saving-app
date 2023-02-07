a7ca3e7255399fa2719adf21cf4013259b5c53e2
bseufw <ChandlerX101@gmail.com>
2023-02-07 13:41:37 -0500

    pushing app

---
+++ website/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import Files from './Files';
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Files/>
+    </div>
+  );
+}
+
+export default App;
