d4f985c244e1ac0888fe84e7eb36936133d773a1
bseufw <ChandlerX101@gmail.com>
2023-02-22 15:43:28 -0500

    pages

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import Files from './Files';
+import Home from './pages/Home';
+import Files from './pages/Files';
+import Layout from './pages/Layout';
+import NoPage from './pages/NoPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home/>
-      <Files/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="files" element={<Files/>} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
